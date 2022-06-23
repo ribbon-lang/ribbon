dd1fb076b2f63065e38b976443a08c6a915a49ee
cheeze2000 <me@cheeze2000.net>
2022-06-23 23:38:13 +0800

    refactor: make class properties private

--- src/main/kotlin/ribbon/cli/AsciiBox.kt
+++ src/main/kotlin/ribbon/cli/AsciiBox.kt
@@ -1,9 +1,9 @@
 package ribbon.cli
 
 class AsciiBox(vararg args: String) {
-	val lines = args.asList()
-	val longestLine = lines.maxByOrNull<String, Int> { it.length }
-	val width = longestLine?.length ?: 0
+	private val lines = args.asList()
+	private val longestLine = lines.maxByOrNull<String, Int> { it.length }
+	private val width = longestLine?.length ?: 0
 
 	override fun toString(): String {
 		val paddedLines = lines.map {
