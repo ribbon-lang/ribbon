9e807e93b181c0e5b61b6ea530a328b803fd10b7
cheeze2000 <me@cheeze2000.net>
2022-06-23 23:44:30 +0800

    test: repl

---
+++ src/test/kotlin/ribbon/cli/ReplTest.kt
@@ -0,0 +1,93 @@
+import com.github.stefanbirkner.systemlambda.SystemLambda.*
+import kotlin.test.*
+
+import ribbon.*
+import ribbon.cli.*
+
+class ReplTest {
+	@Test
+	fun testReplWithoutInput() {
+		withTextFromSystemIn().execute {
+			val expected = buildString {
+				appendLine(Constants.REPL_WELCOME_MSG)
+				append(Constants.REPL_PROMPT)
+			}
+
+			val actual = tapSystemOut {
+				main()
+			}
+
+			assertEquals(expected, actual)
+		}
+	}
+
+	@Test
+	fun testReplHelpCommand() {
+		withTextFromSystemIn(Constants.REPL_HELP_CMD).execute {
+			val expected = buildString {
+				appendLine(Constants.REPL_WELCOME_MSG)
+				append(Constants.REPL_PROMPT)
+				appendLine(Constants.REPL_HELP_MSG)
+				append(Constants.REPL_PROMPT)
+			}
+
+			val actual = tapSystemOut {
+				main()
+			}
+
+			assertEquals(expected, actual)
+		}
+	}
+
+	@Test
+	fun testReplQuitCommand() {
+		withTextFromSystemIn(Constants.REPL_QUIT_CMD, Constants.REPL_HELP_CMD).execute {
+			val expected = buildString {
+				appendLine(Constants.REPL_WELCOME_MSG)
+				append(Constants.REPL_PROMPT)
+			}
+
+			val actual = tapSystemOut {
+				main()
+			}
+
+			assertEquals(expected, actual)
+		}
+	}
+
+	@Test
+	fun testReplMultipleCommands() {
+		withTextFromSystemIn(Constants.REPL_HELP_CMD, Constants.REPL_QUIT_CMD, Constants.REPL_HELP_CMD).execute {
+			val expected = buildString {
+				appendLine(Constants.REPL_WELCOME_MSG)
+				append(Constants.REPL_PROMPT)
+				appendLine(Constants.REPL_HELP_MSG)
+				append(Constants.REPL_PROMPT)
+			}
+
+			val actual = tapSystemOut {
+				main()
+			}
+
+			assertEquals(expected, actual)
+		}
+	}
+
+	@Test
+	fun testReplNonCommandsEchoed() {
+		withTextFromSystemIn("Hello, Ribbon!").execute {
+			val expected = buildString {
+				appendLine(Constants.REPL_WELCOME_MSG)
+				append(Constants.REPL_PROMPT)
+				appendLine("You said: Hello, Ribbon!")
+				append(Constants.REPL_PROMPT)
+			}
+
+			val actual = tapSystemOut {
+				main()
+			}
+
+			assertEquals(expected, actual)
+		}
+	}
+}
